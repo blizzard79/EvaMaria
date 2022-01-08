a671caa92a96196be0733409b51140428b223203
blizzard79 <96640012+blizzard79@users.noreply.github.com>
2022-01-08 10:47:54 +0630

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -642,7 +642,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"Here is what i found for your query {search}"
+        cap = f"ဒါ{search}လားမသိဘူး"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap, reply_markup=InlineKeyboardMarkup(btn))
